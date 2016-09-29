c240c1ecd7bc5227ab9af87eb0b4dd96e4c7363a
Michael Comella <michael.l.comella@gmail.com>
2016-09-28 17:40:29 -0700

    add fs.get_keys fn.

--- fs.py
+++ fs.py
@@ -1,5 +1,4 @@
-import json
-from util import KEY_FILE
+import util
 
 URL_SEARCH = 'https://api.foursquare.com/v2/venues/search'
 URL_EXPLORE = 'https://api.foursquare.com/v2/venues/explore'
@@ -7,8 +6,7 @@ URL_EXPLORE = 'https://api.foursquare.com/v2/venues/explore'
 _VERSION = '20160928'
 _MODE = 'foursquare'
 
-with open(KEY_FILE) as f:
-    _keys = json.load(f)['foursquare']
+_keys = util.get_keys('foursquare')
 assert 'client_id' in _keys
 assert 'client_secret' in _keys
 
--- util.py
+++ util.py
@@ -1,8 +1,13 @@
+import json
 import requests
 
-KEY_FILE = 'keys.json'
+_KEY_FILE = 'keys.json'
 
 FERRY_BUILDING_LL = '37.795,-122.396'
 
 def get(url, params):
     return requests.get(url, params).json()['response']
+
+def get_keys(key_type):
+    with open(_KEY_FILE) as f:
+        return json.load(f)[key_type]
